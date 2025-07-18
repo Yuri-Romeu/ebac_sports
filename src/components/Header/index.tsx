8e3c4743508a0c4f91394979214889146dd3fa0f
Yuri Romeu <yuriromeu2007@gmail.com>
2025-07-18 09:34:56 -0300

    implementação do redux

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,33 @@
-import * as S from './styles'
+import { useSelector } from 'react-redux';
+import * as S from './styles';
 
-import { Produto } from '../../App'
+import cesta from '../../assets/cesta.png';
+import { paraReal } from '../Produto';
+import { RootReducer } from '../../store';
 
-import cesta from '../../assets/cesta.png'
-import { paraReal } from '../Produto'
+const Header = () => {
+  const itens = useSelector((state: RootReducer) => state.carrinho.itens);
+  const itensFavoritos = useSelector(
+    (state: RootReducer) => state.favoritos.itens
+  );
 
-type Props = {
-  itensNoCarrinho: Produto[]
-  favoritos: Produto[]
-}
-
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
-    acc += item.preco
-    return acc
-  }, 0)
+  const valorTotal = itens.reduce((acc, item) => {
+    acc += item.preco;
+    return acc;
+  }, 0);
 
   return (
     <S.Header>
       <h1>EBAC Sports</h1>
       <div>
-        <span>{favoritos.length} favoritos</span>
+        <span>{itensFavoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
